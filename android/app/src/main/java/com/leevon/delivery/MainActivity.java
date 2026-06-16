07c6b6d4ff07adf5294b429704a0eb2307a4a922
Omnia771148 <omnia771148@gmail.com>
2026-06-16 11:39:42 +0530

    fix: register native plugin before super.onCreate

--- android/app/src/main/java/com/leevon/delivery/MainActivity.java
+++ android/app/src/main/java/com/leevon/delivery/MainActivity.java
@@ -6,7 +6,7 @@ import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         registerPlugin(NativeSettingsPlugin.class);
+        super.onCreate(savedInstanceState);
     }
 }
